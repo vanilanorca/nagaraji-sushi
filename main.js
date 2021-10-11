5cb62c945b16142b301560f0d1f00dc014478765
Vinicius Lopes Alves <->
2021-10-11 13:29:41 -0300

    first

---
+++ main.js
@@ -0,0 +1,24 @@
+// LOADING
+function load (){
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("show").style.display = "flex"
+}
+
+window.onload = (function () { window.setInterval('load()',3500);})
+
+
+
+// ======== HAMBURGER MENU =========
+var hambMenu = document.getElementById("hamb-menu");
+var closeMenu = document.getElementById("close-menu");
+var nav = document.getElementById("nav");
+
+function openMenu(){
+    nav.style.display = "flex"
+    closeMenu.style.display = "inherit"
+}
+
+function closeMenuFunction(){
+    nav.style.display = "none"
+    hambMenu.style.display = "inherit"
+}
